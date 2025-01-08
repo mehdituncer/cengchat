b3cd9e5669fc695f1c4c99593a5e904b97bec5a5
Mehdi Tunçer <36051416+mehdituncer@users.noreply.github.com>
2025-01-08 10:48:31 +0300

    Update axios.js
    "https://cengchat.onrender.com/", eklendi

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "https://cengchat.onrender.com/"," ? "http://localhost:5001/api" : "/api",
   withCredentials: true,
 });
