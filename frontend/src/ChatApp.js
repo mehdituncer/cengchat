9a36147af4e5be33b69434b84f08f4aed8e184bd
Mehdi Tunçer <36051416+mehdituncer@users.noreply.github.com>
2025-01-08 10:46:35 +0300

    Update ChatApp.js
    "https://cengchat.onrender.com/", eklendi

--- frontend/src/ChatApp.js
+++ frontend/src/ChatApp.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5001", {
+const socket = io("http://localhost:5001", "https://cengchat.onrender.com/", {
   query: { userId: "yourUserId" },
 });
 
