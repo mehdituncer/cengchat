8cfad3c265282fa027a78ab8330c8b36c9515352
Mehdi Tunçer <36051416+mehdituncer@users.noreply.github.com>
2025-01-08 10:47:49 +0300

    Update socket.js
    "https://cengchat.onrender.com/", eklendi

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost:5001"], // Gerekirse diğer portları ekleyin
+    origin: ["http://localhost:5173", "http://localhost:5001","https://cengchat.onrender.com/"], // Gerekirse diğer portları ekleyin
   },
 });
 
